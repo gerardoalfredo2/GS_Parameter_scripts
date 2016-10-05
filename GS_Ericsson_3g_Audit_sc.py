02d30588c17972a7137d325d3256a508579b03d5
Gerardo Alfredo Alarcon Rivas <gerardoalfredo2@gmail.com>
2016-10-05 17:37:42 -0500

    Initial Script

---
+++ GS_Ericsson_3g_Audit_sc.py
@@ -0,0 +1,7 @@
+def GS_Audit_Ericsson3g(par_file, input_folder):
+    pass
+
+
+GS_Audit_Ericsson3g(
+    'C:\Users\VervebaMX2\Documents\Projects\Python Scripts\CM&PM Parsers\GS Parameters Audit\CrossCheck3G_ericsson_v2.xlsx',
+    'C:\\Users\\VervebaMX2\\Documents\\Projects\\XML_Parsing\\TEst\\Test_allEricssonXML3G\\Results\\')
