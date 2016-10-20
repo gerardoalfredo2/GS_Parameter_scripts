1c6618de67e2b6626dd3670b48fa800fb4751aff
Gerardo Alfredo Alarcon Rivas <gerardoalfredo2@gmail.com>
2016-10-20 16:39:35 -0500

    The Script  create a column with the "Sitio" and from there make a join with the table of sites and fill this with that information.

--- GS_Ericsson_3g_Audit_sc.py
+++ GS_Ericsson_3g_Audit_sc.py
@@ -1,9 +1,13 @@
 def GS_Audit_Ericsson3g(par_file, input_folder):
     import pandas
     import os.path
+    import datetime
     import numpy as np
+    capitalizer = lambda x: x.upper()
     df = pandas.read_excel(open(par_file, 'rb'), sheetname='Parameter_list')
     parameters = df.set_index('ID')
+    cellsites = pandas.read_excel(open(par_file, 'rb'), sheetname='3gCells')
+
     directory_files = df['File'].tolist()
     directory_files = list(set(directory_files))
     col = ['RNC_name','Id','Id_2','Actual_Value','Correct_Value','Parameter_name']
@@ -30,7 +34,7 @@ def GS_Audit_Ericsson3g(par_file, input_folder):
             par_to_update = (df[df['File'] == tab_name])['ID'].tolist()
             #print par_to_update
             #Create a pandas dataframe with the value of the parameters
-            '''
+
             for para in par_to_update:
                 #print para
                 par_fullname = para
@@ -38,7 +42,7 @@ def GS_Audit_Ericsson3g(par_file, input_folder):
                 parameters_name = str(parameters.loc[par_fullname , 'Parameter_name'])
                 #print parameters_name
                 GS_value = str(parameters.loc[par_fullname, 'GS_OSS_val'])
-                GS_value=str.split(GS_value)
+                GS_value=str.split(GS_value,',')
 
                 #print type(GS_value)
                 if str(GS_value[0]) == '-':
@@ -77,16 +81,19 @@ def GS_Audit_Ericsson3g(par_file, input_folder):
                         parameters.loc[par_fullname, 'Discrepances'] = ou_std
                         parameters.loc[par_fullname, 'Perc_Discrepances'] = 100 * (float(dtro_std) / float(total_count))
                         #print "Dentro de Standard: %s\n Fuera de standard: %s\n Porcentaje: %s" % (dtro_std, ou_std,100*( float(dtro_std) / float(total_count)))
-            '''
+
             for para in par_to_update:
                 #print para
                 par_fullname = para
-                print '\npa_fullname: ', par_fullname
+                #print '\npa_fullname: ', par_fullname
                 parameters_name = str(parameters.loc[par_fullname, 'Parameter_name'])
-                print 'parameter_name: ',parameters_name
+                #print 'parameter_name: ',parameters_name
                 GS_value = str(parameters.loc[par_fullname, 'GS_OSS_val'])
-                GS_value = str.split(GS_value)
-                print GS_value
+                GS_value = str.split(GS_value , ',')
+                GS_value = [str(x) for x in GS_value]
+                #GS_value = [x.upper() for x in GS_value]
+
+                #print GS_value
                 if str(GS_value[0]) == '-':
                     pass
 
@@ -94,33 +101,82 @@ def GS_Audit_Ericsson3g(par_file, input_folder):
                     #print par_celdas
 
                     values_compare = pandas.Series(GS_value)
-                    print 'Values to Compare',values_compare
+                    #print 'Values to Compare',values_compare
                     list_par=table_audit[parameters_name]
                     temp=table_audit[table_audit.columns[0:4]]
                     temp.columns = ['Date', 'RNC_name', 'Id','Id_2']
-                    temp['Actual_Value'] = list_par.values
+                    temp['Actual_Value'] = list_par
                     temp = temp.astype(str)
-                    print 'temp: ', type(temp)
+                    #par_celdas['Actual_Value'] = map(lambda x: x.upper(), par_celdas['Actual_Value'])
+                    #print 'temp: ', type(temp)
                     #print temp
+
 
                     complains = temp[temp['Actual_Value'].isin(values_compare) == False]
 
-                    complains['Correct_Value'] = ','.join(GS_value)
+                    complains['Correct_Value'] = str(','.join(GS_value))
+                    complains['Parameter_name'] = str(par_fullname)
 
+                    names_op = ['IUSA', 'NEXTEL', 'ATT']
+                    for ind in complains.index:
 
-                    complains['Parameter_name'] = par_fullname
-                    print complains
+                        print str(ind)
+                        # print str(par_celdas.loc[ind, 'Id'])
+                        if str(complains.loc[ind, 'Id']) == str(complains.loc[ind, 'RNC_name']):
+                            complains.loc[ind, 'Cell_name'] = ''
+                        elif str(complains.loc[ind, 'Id']) == str(complains.loc[ind, 'Id_2']):
+                            complains.loc[ind, 'Cell_name'] = complains.loc[ind, 'Id']
+
+                        elif '-' in str(complains.loc[ind,'Id']):
+                            print 'neighbor',complains.loc[ind,'Id']
+                            variab= str(list(str(complains.loc[ind,'Id']).split('-'))[0])[:-1]
+                            complains.loc[ind,'Cell_name'] = variab
+                        elif '_' in list(complains.loc[ind, 'Id']):
+                            complains.loc[ind, 'Cell_name'] = str(complains.loc[ind, 'Id_2'])[:-1]
+                        elif str(complains.loc[ind, 'Id']) == '1':
+                            complains.loc[ind, 'Cell_name'] = str(complains.loc[ind, 'Id_2'])[:-1]
+                        else:
+                            complains.loc[ind, 'Cell_name'] = str(complains.loc[ind, 'Id'])[:-1]
+                        print 'index: ', ind
+                        print str(complains.loc[ind, 'Cell_name'])
+
+                    #print complains
                     par_celdas = [par_celdas, complains]
                     par_celdas = pandas.concat(par_celdas)
-                ''''''
+    #temporal
 
 
-    print par_celdas
+    #print par_celdas
     #print parameters
-    writer = pandas.ExcelWriter(input_folder+'CM_Parameter_Audit.xlsx')
-    parameters.to_excel(writer, 'Parameter_audit')
+
+
+    print 'Starting slowthing'
+    print "Total of elements are ",len(par_celdas.index)
+    par_celdas.rename(columns={'Cell_name': 'Sitio'}, inplace=True)
+
+    print cellsites
+    print par_celdas
+    par_celdas= par_celdas[['Date', 'RNC_name', 'Sitio', 'Id','Id_2','Parameter_name','Actual_Value','Correct_Value']]
+    par_celdas = par_celdas.merge(cellsites, on='Sitio', how='inner')
+    #par_celdas = pandas.merge(par_celdas, cellsites, on='Cell_name')
+    #par_celdas = pandas.concat([par_celdas, cellsites], axis=1, join='outer')
+
+
+
+
+    #par_celdas['Actual_Value']=par_celdas['Actual_Value'].str.capitalize()
+    par_celdas['Actual_Value']=map(lambda x: x.upper(), par_celdas['Actual_Value'])
+    #par_celdas['Correct_Value'] = par_celdas['Correct_Value'].str.capitalize()
+    par_celdas['Correct_Value']=map(lambda x: x.upper(), par_celdas['Correct_Value'])
+    par_celdas=par_celdas.query('Actual_Value != Correct_Value')
+
+
+    writer = pandas.ExcelWriter(input_folder+'CM_Parameter_Audits.xlsx')
+    parameters.to_excel(writer, 'Parameter_audit', index=False)
     par_celdas.to_excel(writer,'Parameter_Audit_Cell', index=False)
     writer.save()
+    print "Proces" \
+          " dfdfdf Finished"
 
 
 
@@ -129,3 +185,9 @@ def GS_Audit_Ericsson3g(par_file, input_folder):
 GS_Audit_Ericsson3g(
     'C:\Users\VervebaMX2\Documents\Projects\Python Scripts\CM&PM Parsers\GS Parameters Audit\CrossCheck3G_ericsson_v2.xlsx',
     'C:\\Users\\VervebaMX2\\Documents\\Projects\\XML_Parsing\\TEst\\Test_allEricssonXML3G\\Results\\')
+
+
+#GS_Audit_Ericsson3g(
+#   'C:\Users\VervebaMX2\Documents\Projects\Python Scripts\CM&PM Parsers\GS Parameters Audit\CrossCheckLTE_ericsson_v2.xlsx',
+#   'C:\\Users\\VervebaMX2\\Documents\\Projects\\XML_Parsing\\TEst\\Test_allEricssonXMLLTE\\Results\\')
+
